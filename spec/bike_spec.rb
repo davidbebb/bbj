2c9e53ace31bc377dce13d7f712bd059a38f37f3
Michael Roger <bodgeuk12@gmail.com>
2015-09-02 18:29:24 +0100

    Post Challenge 1 Complete

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,6 +1,6 @@
 require "bike"
 
 describe Bike do 
-	it { should respond_to (:working?) }
+	it { is_expected.to respond_to (:working?) }
 	
 end 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,16 +2,23 @@
 require "docking_station"
 
 describe DockingStation do
-	it {should respond_to (:release_bike)}
-	
-end
+	it { is_expected.to respond_to (:release_bike) }
 
-describe DockingStation do 
-	it "Docking station gets a working bike" do
-		bike = DockingStation.new.release_bike
-		bike.class.should == Bike
-		bike.should be_working
+
+	it 'releases working bikes' do
+		subject.dock Bike.new
+		bike = subject.release_bike
+		expect(bike).to be_working
 	end
- 
+
+	it { is_expected.to respond_to(:dock).with(1).argument }
+
+	describe '#release_bike' do
+		it 'raises an error when there are no bikes available' do
+			expect { subject.release_bike }.to raise_error 'No bikes available'
+		end
+	end
+
+
 end 
 
