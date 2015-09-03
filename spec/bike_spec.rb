4f56db283619a623f2ce1766801bf25049efc2f7
Michael Roger <bodgeuk12@gmail.com>
2015-09-03 16:25:22 +0100

    Report a broken bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,5 +2,14 @@ require "bike"
 
 describe Bike do 
 	it { is_expected.to respond_to (:working?) }
+
+	it { is_expected.to respond_to :report_broken }
+
+	describe '#report_broken' do
+		it do
+			subject.report_broken
+			expect(subject).not_to be_working
+		end
+	end
 	
 end 
